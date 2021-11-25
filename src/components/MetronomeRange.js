56b30f45a40627e9399616fbd030fc8480452a31
Craig Marshall <dev.craigmarshall@gmail.com>
2021-11-25 14:55:26 +0000

    Added repetition

--- src/components/MetronomeRange.js
+++ src/components/MetronomeRange.js
@@ -17,14 +17,25 @@ const MetronomeRange = () => {
     
     const [isPlaying, setIsPlaying] = useState(null)
     const [sliderValue, setSliderValue] = useState(50)
+    const [intervalID,setIntervalID] = useState(null)
 
     const click1 = new Audio("https://daveceddia.com/freebies/react-metronome/click1.wav");
 
     useEffect(() => {
         
-        if (isPlaying === null) return
-        isPlaying ? click1.play() : click1.pause()
       }, [isPlaying])
+
+
+    const startInterval = () => {
+        let BPM = (1000 * 60) / sliderValue
+        const startMetronome = setInterval(function(){ click1.play(); }, BPM )
+        setIntervalID(startMetronome)
+    }
+    
+    const stopInterval = () => {
+        clearInterval(intervalID)
+        setIntervalID(null)
+    }
 
     const handlePlayPause = () => {
         // playSound()
@@ -44,6 +55,7 @@ const MetronomeRange = () => {
 
     const handleSliderMove = function(newValue) {
         setSliderValue(newValue)
+        // calcluateInterval = (newValue / 60) * 1000
     }
 
     
@@ -52,22 +64,23 @@ const MetronomeRange = () => {
         <div className="range-container">
             <div className="range-header">
             <h1>{sliderValue}<span className="bpm">BPM</span></h1>
-            <img
+            {/* <img
                 className="play-button"
                 src='https://image.freepik.com/free-icon/play-button_318-42541.jpg'
                 onClick={handlePlayPause}
-            />
+            /> */}
             </div>
             <div className="range-content">
-            <button onClick={handlePlayPause}>{isPlaying ? "Stop" : "Start"}</button>
+            <button onClick={startInterval}>Start</button>
+            <button onClick={stopInterval}>Stop</button>
             {/* <img onClick={handleMinusBPM} src="https://static.thenounproject.com/png/261368-200.png" width="25px"></img> */}
 
 
 
-            <input onChange={(event) => handleSliderMove(event.target.value)} type="range" min="0" max="100" defaultValue={sliderValue} id="myRange"></input>
+            <input onChange={(event) => handleSliderMove(event.target.value)} type="range" min="40" max="240" defaultValue={sliderValue} id="myRange"></input>
 
 
-            <img onClick={handleAddBPM} src="https://static.thenounproject.com/png/645446-200.png" width="25px"></img>
+            {/* <img onClick={handleAddBPM} src="https://static.thenounproject.com/png/645446-200.png" width="25px"></img> */}
             </div>
         </div>
         
